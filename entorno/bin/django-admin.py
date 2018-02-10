4825ed46e393d9dfc46e5718f97f5f0edcea399c
YJC <yjc1984@gmail.com>
2018-02-10 15:43:17 -0500

    Version inicial Web Concursos

---
+++ entorno/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/YJC/Desktop/Uniandes/Maestria/AplicacionesCloud/Proyectos/Proyecto_1/entorno/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
